bd46a888f32ad489bb464e01618c2a5bd9906367
Harvie Hutchinson <163534@chrome.escg.ac.uk>
2024-06-07 08:52:08 +0100

    v18

--- Assets/Scripts/EnemyScripts/Flocking/GlobShotScript.cs
+++ Assets/Scripts/EnemyScripts/Flocking/GlobShotScript.cs
@@ -31,7 +31,8 @@ public class GlobShotScript : MonoBehaviour
 
     // Update is called once per frames
     void EvaluateSlerpPoints()
-    { 
+    {
+
         Vector3 centerPivot = (start + end) * 0.5f;
 
         centerPivot -= new Vector3(0, 0.5f, 0);
@@ -45,31 +46,23 @@ public class GlobShotScript : MonoBehaviour
         transform.position += centerPivot;
 
     }
-    void Slerp()
-    {
-        // The center of the arc
-        Vector3 center = (start + end) * 0.5F;
-
-        // move the center a bit downwards to make the arc vertical
-        center -= new Vector3(0, 1, 0);
-
-        // Interpolate over the arc relative to center
-        Vector3 riseRelCenter = start - center;
-        Vector3 setRelCenter = end - center;
-
-        // The fraction of the animation that has happened so far is
-        // equal to the elapsed time divided by the desired time for
-        // the total journey.
-        float fracComplete = (Time.time - startTime) / journeyTime;
-
-        transform.position = Vector3.Slerp(riseRelCenter, setRelCenter, fracComplete);
-        transform.position += center;
-    }
+   
     private void OnCollisionEnter(Collision col)
     {
+        Destroy(gameObject);
         if(col.gameObject.CompareTag("Player"))
         {
             print("Hit player");
+            Destroy(gameObject);
+        }
+        if (col.gameObject.tag == "Ground" )
+        {
+            print("Hit environment");
+            Destroy(gameObject);
+        }
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            print("Hit environment");
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/MenuScripts/AudioManager.cs
+++ Assets/Scripts/MenuScripts/AudioManager.cs
@@ -26,7 +26,7 @@ public class AudioManager : MonoBehaviour
     private void Start()
     {
 
-        PlayMusic("Background");
+        PlayMusic("MainMenuMusic");
     }
     public void PlayMusic(string name)
     {
